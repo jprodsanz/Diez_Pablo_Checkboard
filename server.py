a2cff05bc36fcb49dd08e4ce39b126c5056ea58a
Pablo X <jprodsanz@gmail.com>
2022-08-24 13:28:53 -0500

    2nd

--- server.py
+++ server.py
@@ -10,15 +10,15 @@ def index():
 def row(x):
     return render_template('index.html',row=x,col=8,color_one='red',color_two='black')
 
-@app.route('/int:x>/<int:y>')
+@app.route('/<int:x>/<int:y>')
 def row_col(x,y):
     return render_template('index.html',row=x, col=y,color_one='red',color_two='black')
 
-@app.route('/int:x>/<int:y>/<string:one>')
+@app.route('/<int:x>/<int:y>/<string:one>')
 def row_col_one(x,y,one):
     return render_template('index.html',row=x, col=y,color_one=one,color_two='black')
 
-@app.route('/int:x>/<int:y>/<string:one>/<string:two>')
+@app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
 def row_col_two(x,y,one,two):
     return render_template('index.html',row=x, col=y,color_one=one,color_two=two)
 
